f80d887cde6071647e7bc9185b9104d4590c0a02
skatox <migueluseche@skatox.com>
2025-07-27 22:08:32 -0500

    chore: rebranding to kim6

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
-# This file is part of Kde Image Menu (KIM). KIM was created by
-# Charles Bouveyron <charles.bouveyron@free.fr>.
+# This file is part of Kde Image Menu for Plasma 6 (KIM6). KIM6 is a fork 
+# from KIM which was created by Charles Bouveyron <charles.bouveyron@free.fr>.
 # 
-# KIM is free software; you can redistribute it and/or modify
+# KIM6 is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # any later version.
 # 
-# KIM is distributed in the hope that it will be useful,
+# KIM6 is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
@@ -39,4 +39,4 @@ cp ChangeLog /usr/share/doc/kim/
 cp README /usr/share/doc/kim/
 
 
-echo "Installation completed. Good bye."
+echo "KIM6 has been installed. Good bye."
